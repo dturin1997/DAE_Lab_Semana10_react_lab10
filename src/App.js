17b67465a38f7839157a417ae7b14524ecc93a7d
Dario Turin <dturin1997@gmail.com>
2022-05-17 17:32:06 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+//function App() {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      productos: []
+    }
+  }
+
+  componentWillMount() {
+    fetch('http://localhost:8000/productos/')
+      .then((response) => {
+        return response.json()
+      })
+      .then((prod) => {
+        this.setState({ productos: prod })
+      })    
+  } 
+
+
+  render() {
+    return (
+      <div>
+        <table border="1">
+        <thead>
+          <tr>
+            <th>Código</th>
+            <th>Descripción</th>
+            <th>Precio</th>                    
+          </tr>
+        </thead>
+        <tbody>  
+          {this.state.productos.map(producto => {
+            return (
+              <tr key={producto.codigo}>
+                <td>{producto.codigo}</td>
+                <td>{producto.descripcion}</td>
+                <td>{producto.precio}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+        </table>
+      </div>
+    );
+  }
+
 }
 
 export default App;
